cdf716191d423a11140e942de9d0492dfcba014f
K Monshi <46969878+Kvahabz1@users.noreply.github.com>
2020-02-11 15:51:05 -0500

    Add files via upload

---
+++ Person Phase4-constructors-taking input/ClassDemo.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class ClassDemo {
+	public static void main(String[] args) {
+		Person a = new Person();
+		
+		System.out.println(a.getName());
+		
+		a.setName("Bob");
+		
+		System.out.println(a.getName());
+		
+		a.setAge(21);
+		
+		System.out.println(a.getAge());	
+		// create object named b with name Kim
+		Person b = new Person("Kim");
+		System.out.println(b.getName());
+		System.out.println(b.getAge());	
+		
+		Person c = new Person("John", 40);
+		System.out.println(c.getName());
+		System.out.println(c.getAge());
+		
+		//Create another person object d, 
+		//ask for user to select name and age
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter the name: ");
+		String name = input.nextLine();
+		System.out.println("Please enter the age: ");
+		int age = input.nextInt();
+		
+		Person d = new Person(name, age);
+		System.out.println(d.getName());
+		System.out.println(d.getAge());
+		
+		int num = 10;
+		System.out.println(num);
+		System.out.println(d);
+		
+		d = new Person("Kim", 4);
+		System.out.println(d);
+
+		input.close();
+	}
+}
---
+++ Person Phase4-constructors-taking input/Person.java
@@ -0,0 +1,55 @@
+public class Person {
+	// fields
+	private String name;
+	private int age;
+
+	// methods
+	//no-arg constructor method
+	public Person()
+	{
+		name = "noname";
+		age = 1;
+	}
+	//parameterized constructor
+	public Person(String name)
+	{
+		this.name = name;
+		age = 1;
+	}
+	// parameterized constructor to set
+	// name and age
+	public Person(String name, int age)
+	{
+		this.name = name;
+		this.age = age;
+	}
+	
+	// setter method
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	// getter method
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+	public int getAge()
+	{
+		return age;
+	}
+	
+	public void SayName() {
+		System.out.println("My name is " + name + " and my age "
+							+ "is " + age);
+	}
+
+	public void HaveBirthday() {
+		age++;
+	}
+}
