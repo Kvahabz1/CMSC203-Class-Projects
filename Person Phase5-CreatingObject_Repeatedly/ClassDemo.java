a28a2a7b2a93ffd73115937ab7e435302410caf0
K Monshi <46969878+Kvahabz1@users.noreply.github.com>
2020-02-12 16:40:45 -0500

    Add files via upload

---
+++ Person Phase5-CreatingObject_Repeatedly/ClassDemo.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ClassDemo {
+	public static void main(String[] args) {
+ 
+		
+		//Add code to create an object repeatedly
+		
+		String name,ans;
+		int age;
+		Person p;
+		Scanner input = new Scanner(System.in);
+		do {
+
+			System.out.println("Enter name :");
+			name = input.nextLine();
+			System.out.println("Enter age :");
+			age = input.nextInt();
+			p = new Person(name,age);
+			System.out.println(p.getName() + 
+					   " You are " +p.getAge() + " Years old");
+			input.nextLine();
+			System.out.println("Do you want to continue?");
+			ans = input.nextLine();
+		}while (ans.equals("y") );
+		
+		input.close();
+	}
+}
