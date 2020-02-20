a66d87c3bb09f32e9e3643284a7c5ba9d4be38e8
K Monshi <46969878+Kvahabz1@users.noreply.github.com>
2020-02-20 11:11:23 -0500

    Update Person.java

--- Person Phase1/Person.java
+++ Person Phase1/Person.java
@@ -1,5 +1,18 @@
+/**
+
+*/
 public class Person {
- String name; int age;
-public void SayName(){ System.out.println("My name is " + name + " and my age is " + age);}
-public void HaveBirthday() { age++;}
+ String name;
+ int age;
+/**
+
+*/
+public void SayName(){
+ System.out.println("My name is " + name + " and my age is " + age);
+}
+/**
+*/
+public void HaveBirthday() {
+ age++;
+}
 }
